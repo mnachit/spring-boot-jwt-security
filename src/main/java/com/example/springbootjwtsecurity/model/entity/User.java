6dfa0fd287506c1ef4c3d59b682ecc16d8b4f704
mnachit <nachitmed70@gmail.com>
2024-06-10 12:19:21 +0100

    Add JWT service implementation

---
+++ src/main/java/com/example/springbootjwtsecurity/model/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.springbootjwtsecurity.model.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Enabled
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String fullName;
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
