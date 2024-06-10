a8189d55d7d467c9b3e68c18a894a860111eef4f
mnachit <nachitmed70@gmail.com>
2024-06-10 10:36:33 +0100

    Add datasource configuration

---
+++ src/test/java/com/example/springbootjwtsecurity/SpringBootJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjwtsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
